0c85aca1685922104151eafa3f53108a838e1918
Steve <lorenz21@hawaii.edu>
2018-04-02 21:16:15 -1000

    created button for magic surge effects

--- wildmagiks/wildmagiks/wildmagiks/urls.py
+++ wildmagiks/wildmagiks/wildmagiks/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('wmsg/', include('wmsg.urls')),
     path('admin/', admin.site.urls),
+    path('', include('wmsg.urls')),
 ]
--- wildmagiks/wildmagiks/wmsg/views.py
+++ wildmagiks/wildmagiks/wmsg/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import WildMagicSurgeEffect
+import random
 
 def index(request):
-    effect = WildMagicSurgeEffect.objects.get(id="2")
-    return HttpResponse(effect)
+    effect = WildMagicSurgeEffect.objects.get(id=random.randint(1,3))
+    # effects = WildMagicSurgeEffect.objects.order_by('id')[:3]
+    return render(request, 'wmsg/index.html', {'effect': effect})
